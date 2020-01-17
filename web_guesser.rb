abb5ba515a1944ec36f748c87d5354e057c52878
Henrik Nørgaard <menneskefernis@gmail.com>
2020-01-17 22:31:55 +0100

    add counter so user has 5 guesses before new number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,33 +1,45 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(1..100)
+@@number = rand(1..100)
 @color = ""
+@@turns_left = 5
 @guessed = false
 
 get '/' do
   guess = params["guess"].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => @color, :guessed => @guessed}
+  message = check_guess(guess) if params["guess"]
+  new_number if @@turns_left <= 0
+  new_number if @guessed
+  erb :index, :locals => {:number => @@number, :message => message, :color => @color, :turns_left => @@turns_left, :guessed => @guessed}
 end
 
 def check_guess(guess)
   case true
-  when guess == SECRET_NUMBER
+  when guess == @@number
     @guessed = true
     @color = "green"
     "You got it right!"
-  when (guess - 5) > SECRET_NUMBER
+  when (guess - 5) > @@number
+    @@turns_left -= 1
     @color = "crimson"
     "Waaay too high!"
-  when guess > SECRET_NUMBER
+  when guess > @@number
+    @@turns_left -= 1
     @color = "lightcoral"
     "Too high!"
-  when (guess + 5) < SECRET_NUMBER
+  when (guess + 5) < @@number
+    @@turns_left -= 1
     @color = "crimson"
     "Waaay too low!"
-  when guess < SECRET_NUMBER
+  when guess < @@number
+    @@turns_left -= 1
     @color = "lightcoral"
     "Too low!"
   end
+end
+
+def new_number
+  @@number = rand(1..100)
+  @@turns_left = 5
 end
